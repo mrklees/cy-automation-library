c2acf9c8964f61baec95e38fc9b410694cd42045
Chris Luedtke <chrisluedtke@gmail.com>
2018-10-17 12:12:30 -0500

    consolidated scripts to simple_cysh

---
+++ cyautomation/cyschoolhouse/add_students_to_cysh.py
@@ -0,0 +1,116 @@
+from cyschoolhousesuite import *
+from selenium.webdriver.support.ui import Select
+from simple_cysh import *
+from time import sleep
+import numpy as np
+
+def import_parameters(xlsx_path, enrollment_date):
+    """Imports input data from xlsx
+    """
+    df = pd.read_excel(xlsx_path, converters={'*REQ* Grade':int})
+
+    column_rename = {
+        'Student CPS ID':'*REQ* Local Student ID',
+        'Student First Name':'*REQ* First Name',
+        'Student Last Name':'*REQ* Last Name',
+        'Student Grade Level':'*REQ* Grade',
+    }
+
+    df.rename(columns=column_rename, inplace=True)
+    
+    df["*REQ* Student Id"] =  df['*REQ* Local Student ID']
+    df["*REQ* Entry Date"] = enrollment_date
+    df["*REQ* Type"] = 'Student'
+    df["Date of Birth"] = np.nan
+    df["Gender"] = np.nan
+    df["Ethnicity"] = np.nan
+    df["Disability Flag"] = np.nan
+    df['ELL'] = np.nan
+
+    col_order = [
+        'School',
+        '*REQ* Student Id',
+        '*REQ* Local Student ID',
+        '*REQ* First Name',
+        '*REQ* Last Name',
+        '*REQ* Grade',
+        'Date of Birth', #nan
+        'Gender', #nan
+        'Ethnicity', #nan
+        'Disability Flag', #nan
+        'ELL', #nan
+        '*REQ* Entry Date',
+        '*REQ* Type',
+    ]
+
+    df = df[col_order]
+    
+    return df
+
+def remove_extant_students(df, cysh):
+    local_student_id_col = '*REQ* Local Student ID'
+    drop_ids = []
+    for index, row in df.iterrows():
+        search_result = cysh.query(f"SELECT Id FROM Student__c WHERE Local_Student_ID__c = '{row[local_student_id_col]}'")
+        
+        if len(search_result['records']) > 0:
+            drop_ids.append(row[local_student_id_col])
+            
+        del search_result
+            
+    df = df.loc[~df[local_student_id_col].isin(drop_ids)]
+    
+    return df
+
+def input_file(driver, path_to_csv):
+    driver.find_element_by_xpath('//*[@id="selectedFile"]').send_keys(path_to_csv)
+    driver.find_element_by_xpath('//*[@id="j_id0:j_id42"]/div[3]/div[1]/div[6]/input[2]').click()
+
+def insert_data(driver):
+    driver.find_element_by_xpath('//*[@id="startBatchButton"]').click()
+    
+# runs the entire script
+def intervention_student_setup(xlsx_dir, xlsx_name, cysh, enrollment_date):
+    xlsx_path = xlsx_dir + xlsx_name
+    
+    params = import_parameters(xlsx_path, enrollment_date)
+    params = remove_extant_students(params, cysh)
+    
+    setup_df = get_cysh_df('Setup__c', ['Id', 'School__c'], rename_id=True, rename_name=True)
+    school_df = get_cysh_df('Account', ['Id', 'Name'])
+    setup_df = setup_df.merge(school_df, how='left', left_on='School__c', right_on='Id'); del school_df
+    setup_df = setup_df.loc[~setup_df['Id'].isnull()]
+    
+    driver = get_driver()
+    open_cyschoolhouse19(driver)
+    
+    for school_name in params['School'].unique():
+        # Write csv
+        path_to_csv = xlsx_dir + f"SY19 New Students for CYSH - {school_name}.csv"
+        df_csv = params.loc[params["School"]==school_name]
+        df_csv.drop(["School"], axis=1, inplace=True)
+        df_csv.to_csv(path_to_csv, index=False, date_format='%m/%d/%Y')
+        
+        # Navigatge to student enrollment page
+        setup_id = setup_df.loc[setup_df['Name']==school_name, 'Setup__c'].values[0]
+        driver.get(f'https://c.na30.visual.force.com/apex/CT_core_LoadCsvData_v2?setupId={setup_id}&OldSideBar=true&type=Student')
+        sleep(2)
+        
+        input_file(driver, path_to_csv)
+        sleep(2)
+        
+        insert_data(driver)
+        sleep(2)
+
+        # Publish
+        # Seems to work, but not completely sure if script 
+        # pauses until upload is complete, both for the "Insert Data"
+        # phase, and the "Publish Staff/Student Records" phase.
+        
+        driver.get(f'https://c.na30.visual.force.com/apex/schoolsetup_staff?setupId={setup_id}')
+        driver.find_element_by_css_selector('input.red_btn').click()
+        sleep(3)
+
+        os.remove(path_to_csv)
+
+    #driver.quit()
--- cyautomation/cyschoolhouse/simple_cysh.py
+++ cyautomation/cyschoolhouse/simple_cysh.py
@@ -59,7 +59,7 @@ def get_cysh_df(sf_object, sf_fields=None, where=None, rename_id=False, rename_n
         if year not in valid_years:
             raise ValueError(f'The year provided ({year}) must be one of: {", ".join(valid_years)}.')
         
-        df = pd.read_csv(f'Z:\\ChiPrivate\\Chicago Data and Evaluation\\Whole Site End of Year Data\\Salesforce Objects\\{year}\\' + sf_object + '.csv')
+        df = pd.read_csv(f'Z:\\ChiPrivate\\Chicago Data and Evaluation\\Whole Site End of Year Data\\Salesforce Objects\\{year}\\{sf_object}.csv')
         if sf_fields is not None:
             df = df[sf_fields]
     
@@ -69,6 +69,84 @@ def get_cysh_df(sf_object, sf_fields=None, where=None, rename_id=False, rename_n
         df.rename(columns={'Name':(sf_object+'_Name')}, inplace=True)
 
     return df
+
+def section_enrollment_sync(source_section, destination_section, enrollment_start_date, ACM_to_TL=False):
+    """ I'm going to document, I promise.
+    """
+    
+    if type(source_section) != str or type(destination_section) != str:
+        raise ValueError(f'source_section and destination_section must be strings.')
+        
+    program_df = get_cysh_df('Program__c', ['Id', 'Name'], rename_id=True, rename_name=True)
+    
+    for x in [source_section, destination_section]:
+        if x not in program_df['Program__c_Name'].tolist():
+            raise ValueError(f'{x} is not a valid section type.')
+    
+    student_section_df = get_cysh_df('Student_Section__c', ['Id', 'Student__c', 'Section__c'])
+    section_df = get_cysh_df('Section__c', ['Id', 'Name', 'School__c', 'Program__c', 'Intervention_Primary_Staff__c'], rename_id=True, rename_name=True)
+    
+    school_df = get_cysh_df('Account', ['Id', 'Name'])
+    school_df = school_df.rename(columns={'Id':'School__c', 'Name':'School'})
+    
+    section_df = section_df.merge(school_df, how='left', on='School__c')
+    section_df = section_df.merge(program_df, how='left', on='Program__c')
+    
+    student_section_df = student_section_df.merge(section_df, how='left', on='Section__c')
+    
+    # get students enrolled in source section
+    source_enrollment_df = student_section_df.loc[student_section_df['Program__c_Name'] == source_section].copy()
+    
+    # get students enrolled in destination section
+    dest_enrollment_df = student_section_df.loc[student_section_df['Program__c_Name'] == destination_section].copy()
+    
+    if ACM_to_TL == True:
+        source_enrollment_df.loc[:,'key'] = source_enrollment_df[['Student__c', 'School']].astype(str).sum(axis=1)
+        source_enrollment_df = source_enrollment_df.drop_duplicates('key')
+        dest_enrollment_df.loc[:,'key'] = dest_enrollment_df[['Student__c', 'School']].astype(str).sum(axis=1)
+    else:
+        source_enrollment_df.loc[:,'key'] = source_enrollment_df[['Student__c', 'Intervention_Primary_Staff__c']].astype(str).sum(axis=1)
+        dest_enrollment_df.loc[:,'key'] = dest_enrollment_df[['Student__c', 'Intervention_Primary_Staff__c']].astype(str).sum(axis=1)
+    
+    # get students enrolled in source section but not destination section
+    to_enroll_df = source_enrollment_df.loc[~source_enrollment_df['key'].isin(dest_enrollment_df['key'])].copy()
+    
+    # get set of destination sections
+    dest_section_df = section_df.loc[section_df['Program__c_Name'] == destination_section].copy()
+    
+    # merge in section ID to enroll in
+    if ACM_to_TL == True:
+        to_enroll_df = to_enroll_df.merge(dest_section_df[['School__c', 'Section__c']], 
+                                          how='left', on='School__c', suffixes=('_from', '_to_Enroll'))
+    else:
+        to_enroll_df = to_enroll_df.merge(dest_section_df[['Intervention_Primary_Staff__c', 'Section__c']], 
+                                          how='left', on='Intervention_Primary_Staff__c', suffixes=('_from', '_to_Enroll'))
+    
+    to_enroll_df = to_enroll_df.loc[~to_enroll_df['Section__c_to_Enroll'].isnull()]
+    
+    print(f"Enrolling {len(to_enroll_df)} students to {destination_section} sections.")
+    
+    results = []
+    for index, row in to_enroll_df.iterrows():
+        result = create_student_section(student__c = row['Student__c'], section__c = row['Section__c_to_Enroll'], 
+                                        enrollment_start_date = enrollment_start_date)
+        results.append(result)
+    
+    return results
+
+
+def create_student_section(student__c, section__c, enrollment_start_date):
+    stu_sect_dict = {
+        'Student__c':student__c,
+        'Intervention_Enrollment_Start_Date__c':enrollment_start_date,
+        'Enrollment_Start_Date__c':enrollment_start_date,
+        'Section__c':section__c,
+    }
+    
+    result = cysh.Student_Section__c.create(stu_sect_dict)
+    
+    return result
+
 
 cysh_object_reference = [
     'Name',
